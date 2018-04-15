7ec4bdd8d49211d5a102b6dc8f9038f38e86cee4
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2018-04-16 01:54:54 +0200

    Initial Commit

---
+++ ScrollUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollUp
+//
+//  Created by Ahmed Khedr on 4/16/18.
+//  Copyright © 2018 Ahmed Khedr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
